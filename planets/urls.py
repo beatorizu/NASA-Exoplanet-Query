27a238a48ededc88e0fc7bfee0b4212bead27ec8
beatorizu <betrizaf@gmail.com>
2024-06-13 14:20:01 -0300

    Implement search view

--- planets/urls.py
+++ planets/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("search/", views.SearchResultView.as_view(), name="search_planets"),
     path("", views.HomePageView.as_view(), name="home"),
 ]
--- planets/views.py
+++ planets/views.py
@@ -1,8 +1,10 @@
 from typing import Any
 
-from django.views.generic import TemplateView
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.views.generic import ListView, TemplateView
 
-from planets.models import Facility, Hostname, Method
+from planets.models import Facility, Hostname, Method, Planet
 
 
 class HomePageView(TemplateView):
@@ -17,3 +19,21 @@ class HomePageView(TemplateView):
             "facilities": Facility.objects.all()
         }
         return context
+
+
+class SearchResultView(ListView):
+    model = Planet
+    template_name = "search_results.html"
+
+    def get_queryset(self) -> QuerySet[Any]:
+        base_filter = Q()
+        if self.request.GET.get("year_of_discovery") is not None:
+            base_filter &= Q(year_of_discovery=self.request.GET["year_of_discovery"])
+        if self.request.GET.get("discovery_method") is not None:
+            base_filter &= Q(discovery_method__name__iregex=self.request.GET["discovery_method"])
+        if self.request.GET.get("hostname") is not None:
+            base_filter &= Q(hostname__name__iregex=self.request.GET["hostname"])
+        if self.request.GET.get("discovery_facility") is not None:
+            base_filter &= Q(discovery_facility__name__iregex=self.request.GET["discovery_facility"])
+
+        return Planet.objects.filter(base_filter)
